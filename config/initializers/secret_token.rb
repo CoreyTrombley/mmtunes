33554df303818cae223ff34e6f247d0e3c57ae45
Corey Trombley <corey@quilible.com>
2012-10-23 08:35:10 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mmtunes::Application.config.secret_token = '5ea31cf783fbc6cd6570afe279184dab517b2f9b1fcd5cc939d4ce0a499ab8cb20c5dd0e8a3490df03de95cf2348afa7d0132eea62374d694f4e0cf6c3419097'
