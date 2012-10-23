6b52c448731d3e673956c35299c7293c5380a102
Corey Trombley <corey@quilible.com>
2012-10-23 09:23:53 -0400

    changes the song model

--- app/models/songs.rb
+++ app/models/songs.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  price      :decimal(, )
+#  file_name  :string(255)
+#  lyrics     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Songs < ActiveRecord::Base
-  attr_accessible :name, :price
+  attr_accessible :file_name, :lyrics, :name, :price
 end
