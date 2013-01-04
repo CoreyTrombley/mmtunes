4054c7b197bd5a20119596a633cc93cb7a0c0d5e
Corey Trombley <corey@quilible.com>
2013-01-04 11:35:13 -0500

    functionality for purchase of songs

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,20 @@
 class PurchasesController < ApplicationController
-  def new
-    @purchase = Purchase.new
-  end
 
   def create
     @purchase = Purchase.new
+    @purchase.song = Song.find(params[:song_id])
+    @purchase.user = current_user
+    @purchase.price_paid = @purchase.song.price
+    if @purchase.save
+      purchaser = @purchase.user_id
+      @user = User.find_by_id(purchaser)
+      balance = @user.balance
+      @user.balance = balance - @purchase.price_paid
+      @user.save
+      redirect_to player_path
+    else
+      render :players
+    end
   end
+
 end
